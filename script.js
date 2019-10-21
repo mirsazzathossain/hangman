82b324dda78ec2021214587ba7d6db40c07ef588
mirsazzathossain <mirsazzathossain@gmail.com>
2019-10-21 22:34:13 +0600

    Updated!!

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ var arr=["education", "technology", "mathematics", "science", "engineering", "ar
 var num = document.getElementById('number').value;
 
 $('.play').on('click', function(){
-    
+    var index = $('#number').val();
+    var topic = arr[index];
+
+    for(var char of topic){
+        console.log(char);
+    }
+
 });
