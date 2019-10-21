370f898e230354a9689c3b0046e0ee283523e4dc
mirsazzathossain <mirsazzathossain@gmail.com>
2019-10-21 22:55:00 +0600

    updated!!

--- script.js
+++ script.js
@@ -7,8 +7,17 @@ $('.play').on('click', function(){
     var index = $('#number').val();
     var topic = arr[index];
 
+    
+    var script='';
     for(var char of topic){
-        console.log(char);
+        if(char == 'a' || char == 'e' || char == 'i' || char == 'o' || char == 'u'){
+           script+= '<div class="p-3 bd-highlight" style="border-bottom: 1px dotted red; margin-right:5px;"></div>';
+        }
+        else{
+            script+= '<div class="p-3 bd-highlight" style="border-bottom: 1px solid black; margin-right:5px;"></div>';
+        }
     }
+
+    $('.output').append(`<div class="d-flex flex-row bd-highlight mb-3">${script}</div>`);
 
 });
