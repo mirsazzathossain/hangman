952235b449edeb5b4e2b804286a40c4754eac9de
cis101 <cis101@secs.iub.edu.bd>
2019-10-21 09:41:49 +0600

    mid!!

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 /* Add your script here */
+var arr=["education", "technology", "mathematics", "science", "engineering", "arithmetic"];
+
+var num = document.getElementById('number').value;
+
+$('.play').on('click', function(){
+    
+});
