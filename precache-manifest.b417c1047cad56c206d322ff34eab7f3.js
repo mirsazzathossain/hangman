a5eb7acffde7e013ec8b32a593ddb061150cd1af
mirsazzathossain <1721809@iub.edu.bd>
2020-10-09 20:20:46 +0600

    Updates

---
+++ precache-manifest.b417c1047cad56c206d322ff34eab7f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d0cf5a0b7b8da9d59c550867d875ac",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "c8e5d227488e47618966",
+    "url": "/hangman/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "33d0303c4ef4196fbd0f",
+    "url": "/hangman/static/js/2.58b4784e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.58b4784e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8e5d227488e47618966",
+    "url": "/hangman/static/js/main.526234c3.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hangman/static/media/logo.5d5d9eef.svg"
+  }
+]);
